8d61dca605592f67ce85cd3f9b0eb3117ca0e4d2
srikar8 <madarapu.srikar@gmail.com>
2019-02-14 01:41:45 +0530

    added wedding images

--- js/Wedding_js.js
+++ js/Wedding_js.js
@@ -9,10 +9,22 @@ function displayAllImages() {
 
 // Here has to be some error!!! //
 
- for (i=0;i<=Image.length;i++) {
+ for (i=0;i<=Image.length;++i) {
 
      document.write("<div class='" + "grid-item" + "' data-order='" + i + "'><img class='"+ "img-responsive'" + "src='" + Image[i] + "' width='100%' /></div>");
 
 }
 
 }
+
+
+//to be updated to below one
+/**const testFolder = 'images/Wedding/';
+const fs = require('fs');
+
+fs.readdir(testFolder, (err, files) => {
+  files.forEach(file => {
+    console.log(file);
+  });
+});**/
+

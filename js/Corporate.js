165af604f307b7ffc8d84f4a1e6b569450f7aed9
srikar8 <madarapu.srikar@gmail.com>
2019-09-03 06:56:04 +0530

    updated corporate

---
+++ js/Corporate.js
@@ -0,0 +1,28 @@
+var Image = new Array(); 
+
+function displayAllImages(corporate, num_of_photos) {
+ for (j=0;j<num_of_photos;++j) {
+ 	Image[j] = corporate+j+".png"; 
+	}
+
+// Here has to be some error!!! //
+
+ for (i=0;i<=Image.length;++i) {
+
+     document.write("<div class='" + "grid-item" + "' data-order='" + i + "'><img class='"+ "img-responsive'" + "src='" + Image[i] + "' width='100%' /></div>");
+
+}
+
+}
+
+
+//to be updated to below one
+/**const testFolder = 'images/Wedding/';
+const fs = require('fs');
+
+fs.readdir(testFolder, (err, files) => {
+  files.forEach(file => {
+    console.log(file);
+  });
+});**/
+

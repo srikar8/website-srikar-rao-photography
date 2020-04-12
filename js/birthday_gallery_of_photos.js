a14ede510b949b59323f854eb24563b33cc87466
srikar8 <madarapu.srikar@gmail.com>
2020-04-12 11:34:36 +0530

    delay 300

--- js/birthday_gallery_of_photos.js
+++ js/birthday_gallery_of_photos.js
@@ -8,7 +8,7 @@ function displaygalleryImages(wedding_name, num_of_photos) {
 // Here has to be some error!!! //
 
  for (i=0;i<=Image.length;++i) {
- 	var str2="<div class='" + "grid-item" + "' data-order='" + i + "' data-aos='" + "fade-up" + "' data-aos-delay='" +"100"+ "'><a href='" + Image[i] + "' class='"+ "d-block photo-item'" + " data-fancybox='" + "gallery" + "'><img src='" + Image[i]+ "' alt='"+"Image"+"' class='"+"img-fluid img-responsive "+"'><div class='"+"photo-text-more"+"'><span class='"+"icon icon-search"+"'></span></div></a></div>";
+ 	var str2="<div class='" + "grid-item" + "' data-order='" + i + "' data-aos='" + "fade-up" + "' data-aos-delay='" +"300"+ "'><a href='" + Image[i] + "' class='"+ "d-block photo-item'" + " data-fancybox='" + "gallery" + "'><img src='" + Image[i]+ "' alt='"+"Image"+"' class='"+"img-fluid img-responsive "+"'><div class='"+"photo-text-more"+"'><span class='"+"icon icon-search"+"'></span></div></a></div>";
  	document.write(str2);
  	console.log(str2);
 
